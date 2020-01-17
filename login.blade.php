b40061ae21079928138e24221177e33333940f6f
fahmifathur13 <fahmi2018rplrus2@gmail.com>
2020-01-17 10:29:11 +0700

    first_tamuprjct

---
+++ login.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header bg-danger text-white ">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+                            <div class="col-md-6">
+                            <input name="username" type="text" class="form-control" id="signin-password" placeholder="Username">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <div class="col-md-6">
+                            <input name="password" type="password" class="form-control" id="signin-password" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-danger">
+                                    {{ __('Login') }}
+                                </button>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ register.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header bg-danger text-white" >{{ __('Register') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="nama" class="col-md-4 col-form-label text-md-right">{{ __('Nama') }}</label>
+                            <div class="col-md-6">
+                            <input name="nama" type="text" class="form-control" id="name" placeholder="nama">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="no_telephone" class="col-md-4 col-form-label text-md-right">{{ __('No Telephone') }}</label>
+                            <div class="col-md-6">
+                            <input name="no_telephone" type="text" class="form-control" id="notlp" placeholder="no telephone">                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+                            <div class="col-md-6">
+                            <input name="username" type="text" class="form-control" id="username" placeholder="username">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <div class="col-md-6">
+                            <input name="password" type="text" class="form-control" id="password" placeholder="password">
+                            </div>
+                        </div>
+
+                        
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-danger">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
